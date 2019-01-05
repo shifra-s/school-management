3247d3b721589fc3b27c0c3b097de442181a969c
shifra <shif602@gmail.com>
2019-01-06 00:03:34 +0200

    fixed comments

--- public/scripts/add-course.js
+++ public/scripts/add-course.js
@@ -4,6 +4,7 @@ $('#add-course').click(function(){
     //reset the error messages in case the user didn't try to submit and the messages remain
     resetCourseFormValidations();
 
+    //show the form
     $('#general-info-school').hide();
     $('#add-student-form').hide();
     $('#student-details-section').hide();
--- public/scripts/add-student.js
+++ public/scripts/add-student.js
@@ -4,12 +4,12 @@ $('#add-student').click(function(){
     //reset the error messages in case the user didn't try to submit and the messages remain
     resetStudentFormValidations();
 
+    //show the form (default is display none)
+
     $('#general-info-school').hide();
     $('#add-course-form').hide();
     $('#student-details-section').hide();
     $('#add-student-form').show();
-    //replace the main container with the form
-    //show the form (default is display none)
 
 });
 
