08662669e667f03ed03e3d5b0c6d1b8636755837
shifra <shif602@gmail.com>
2019-01-06 02:05:07 +0200

    fixed admin role checks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,24 +8,24 @@
                     <div class="panel-heading">{{ \Auth::check() ? 'Welcome, ' . \Auth::user()->name . '!': 'Welcome to JB Management: A Platform for Managing Students, Administrators, and Courses'}}
                     </div>
                     <div class="panel-body">
-                        {{--{{ \Auth::check() ? 'Using this platform, you can manage courses and student registration.' : ''}}--}}
-
-                        @if(\Auth::check()==1)
-                            <p> Using this platform, you can manage different things at JB! As an owner, you can manage the different administrators, as well as the
+                        @if(\Auth::user()->role ==1)
+                            <p> Using this platform, you can manage different things at JB! As an owner, you can manage
+                                the different administrators, as well as the
                                 course list, and student enrollment and registration.</p>
-                            <p>Click on the "Schools" tab in order to manage students as courses. Click on the "administrators" tab in order to manage the administrators at the school.</p>
+                            <p>Click on the "Schools" tab in order to manage students as courses. Click on the
+                                "administrators" tab in order to manage the administrators at the school.</p>
 
 
-                        @elseif(\Auth::check()==2)
+                        @elseif(\Auth::user()->role ==2)
                             <p> As a manager, you can manage the different administrators at JB, as well as the
                                 course list, and student enrollment and registration.</p>
 
-                            <p>Click on the "Schools" tab in order to manage students as courses. Click on the "administrators" tab in order to manage the administrators at the school.</p>
+                            <p>Click on the "Schools" tab in order to manage students as courses. Click on the
+                                "administrators" tab in order to manage the administrators at the school.</p>
 
-
-
-                        @elseif(\Auth::check()==3)
-                            <p> As a sales manager, you can manage the different courses offered at JB, as well as student
+                        @elseif(\Auth::user()->role ==3)
+                            <p> As a sales manager, you can manage the different courses offered at JB, as well as
+                                student
                                 enrollment and registration.</p>
 
                             <p>To do so, click on the "Schools" tab.</p>
